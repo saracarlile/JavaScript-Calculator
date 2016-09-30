cb9f54c84191e7f46d0e6bae563aabe9b67c3a6e
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-09-30 15:59:17 -0500

    working on cal js

--- js/script.js
+++ js/script.js
@@ -7,17 +7,17 @@ $(document).ready(function () {
         else if (elements[i].id === 'CE') {
             elements[i].addEventListener("click", funcCE, false);
         }
-        else if (elements[i].id === 'divide') {
-            elements[i].addEventListener("click", identifyOperator, false);
-        }
-        else if (elements[i].id === 'multiply') {
+        else if (elements[i].id === 'divide' || elements[i].id === 'multiply') {
             elements[i].addEventListener("click", identifyOperator, false);
         }
         else if (elements[i].id === 'subtract') {
-            elements[i].addEventListener("click", identifyOperator, false);
+            elements[i].addEventListener("click", subtract, false);
         }
         else if (elements[i].id === 'plus') {
-            elements[i].addEventListener("click", identifyOperator, false);
+            elements[i].addEventListener("click", plus, false);
+        }
+        else if (elements[i].id === 'period') {
+            elements[i].addEventListener("click", decimal, false);
         }
         else if (elements[i].id === 'equals') {
             elements[i].addEventListener("click", equals, false);
@@ -28,7 +28,7 @@ $(document).ready(function () {
     }
 
     var result = '';
-    var operator = ' ';
+    var st = [];
     var resultDisplay = document.getElementById('result');
 
     function funcAC() {
@@ -38,15 +38,34 @@ $(document).ready(function () {
         result = '';
         resultDisplay.textContent = result;
     }
-     function identifyOperator() {
-         var op = this.textContent;
-         return op;        
+    function identifyOperator() {
+        var op = this.textContent;
+        return st.push(op);
     }
-     
+    function subtract() {
+        var sub = "-";
+        console.log(st);
+        return st.push(sub);
+    }
+
+    function plus() {
+        var pls = "+";
+        console.log(st);
+        return st.push(pls);
+    }
+
+    function decimal() {
+    
+    }
+
     function funcNumber() {
-
+        var num = this.textContent;
+         console.log(st);
+        return st.push(num);
     }
 
+  // var st= 2 + operator + 5;
+  // console.log(eval(st));
 
 
 
