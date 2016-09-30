bed5bca3f0fc0faed123194a557a58f67e2c851a
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-09-30 11:23:29 -0500

    remove js styles

--- js/script.js
+++ js/script.js
@@ -1,27 +1,41 @@
 $(document).ready(function () {
     var elements = document.getElementsByClassName('btn-lg');
     for (var i = 0; i < elements.length; i++) {
-        if (elements[i].id === 'AC' || elements[i].id === 'CE') {
-            elements[i].addEventListener("mouseenter", changeBgDarkRed, false);
-            elements[i].addEventListener("mouseleave", changeBgDarkRedLeave, false);
+        if (elements[i].id === 'AC') {
+            elements[i].addEventListener("click", funcAC, false);
+        }
+        else if (elements[i].id === 'CE') {
+            elements[i].addEventListener("click", funcCE, false);
+        }
+        else if(elements[i].id === 'divide'){
+            elements[i].addEventListener("click", divide, false);
+        }
+         else if(elements[i].id === 'multiply'){
+             elements[i].addEventListener("click", multiply, false);
+        }
+         else if(elements[i].id === 'subtract'){
+             elements[i].addEventListener("click", subtract, false);
+        }
+        else if(elements[i].id === 'plus'){
+            elements[i].addEventListener("click", plus, false);
         }
         else {
-            elements[i].addEventListener("mouseenter", changeBg, false);
-            elements[i].addEventListener("mouseleave", changeBgLeave, false);
+            elements[i].addEventListener("click", funcNumber, false);
         }
     }
-    function changeBgDarkRed() {
-        this.style.background = "#520b0a";
+
+
+    function funcAC() {
+
     }
-    function changeBg() {
-        this.style.background = "#757575";
-        this.style.color = "#FFFFFF";
+    function funcCE() {
+
     }
-     function changeBgDarkRedLeave() {
-        this.style.background = "#a72d45";
+    function funcNumber() {
+
     }
-    function changeBgLeave() {
-        this.style.background = "#BDBDBD";
-        this.style.color = "#000000";
-    }
+
+
+
+
 });
